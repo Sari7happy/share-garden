feeea68db8987c733fc7c726159d11b97dab72c2
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-11-29 10:31:42 +0900

    feature registermigrationファイル作成

---
+++ src/database/migrations/2022_11_28_031047_create_registers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // $table->integer('prefecture_id');
+            // $table->integer('genle_id');
+            $table->integer('reservation_number_of_people');
+            $table->dateTime('reservation_date');
+            $table->string('image_path');
+            $table->dateTime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registers');
+    }
+}
