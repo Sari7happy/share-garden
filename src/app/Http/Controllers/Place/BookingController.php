f049059c43964d015bbc3bf5c55e036ee318a41a
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-12-05 11:27:46 +0900

    feature 予約コントローラー追加

---
+++ src/app/Http/Controllers/Place/BookingController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Place;
+use App\Models\Register;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $register_data = Register::all();
+        return view('UserView.index', compact('register_data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('UserView.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $register_data = Register::where('id', $id)->first();
+        //dd($register_data);
+        return view('UserView.show', compact('register_data'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
