34b0280eca5873a5c40d9801910c59cd0d104120
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-12-05 11:30:48 +0900

    feature フォーム修正

--- src/resources/views/AdminView/create.blade.php
+++ src/resources/views/AdminView/create.blade.php
@@ -5,28 +5,28 @@
 <h2>投稿フォーム</h2>
 
 <!-- 外枠 -->
-<div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px; background-color: lightgray; color: black;">
+<div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px; background-color: lightgray; color: black;width:800px;">
     <form method="post" action="{{ route('reservation.store') }}" enctype="multipart/form-data">
         @csrf
-        <div class="col-md-7">
+        <div class="col-md-6">
             <label class="form-label">場所</label>
             <input type="text" name="prefecture_name" class="form-control" placeholder='都道府県を入力してください' required>
         </div>
         <!-- 県名 -->
         <br />
         <br />
-        <div class="col-md-7">
+        <div class="col-md-3">
             <label class="form-label">ジャンル</label>
             <input type="text" name="genle_name" class="form-control" required>
         </div>
         <br />
 
-        <div class="col-md-7">
+        <div class="col-md-3">
             <label class="form-label">人数</label>
             <input type="text" name="reservation_number_of_people" class="form-control">
         </div>
         <br />
-        <div class="col-md-7">
+        <div class="col-md-3">
             <label class="form-label">日付</label>
             <input type="date" name="reservation_date" class="form-control">
         </div>
