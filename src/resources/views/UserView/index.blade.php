d878a2eadfef87388f868032f60783701a787433
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-12-07 13:49:49 +0900

    feature 検索ビュー作成

--- src/resources/views/UserView/index.blade.php
+++ src/resources/views/UserView/index.blade.php
@@ -2,7 +2,16 @@
 @extends('footer.users_footer')
 @section('content')
 <h1>レジャー一覧</h1>
+<form method="GET" action="{{ route('booking.index') }}">
 
+    <input type="search" placeholder="場所を入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
+    <br />
+    <br />
+    <input type="search" placeholder="ジャンルを入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
+    <div>
+    <input type="submit" value="検索する" class="btn btn-success">
+    </div>
+</form>
 <div class="card-wrapper" style="display:flex;justify-content:center;">
     <div class="card-container" style="display:flex;gap:40px;width:1000px;flex-wrap:wrap;">
 
@@ -10,11 +19,11 @@
         <div class="card" style="width: 200px;display:flex;align-items: center;">
             <img src="{{ Storage::url($post_data->image_path) }}" style="width:100px;" />
             <div class="card-body">
-                <div class="">場所:{{ $post_data->prefecture_name}}</div>
+                <div class="">場所:{{$post_data->prefecture_name}}</div>
                 <div class="">ジャンル:{{$post_data->genle_name }}</div>
                 <br />
 
-                <a href="{{ route('booking.show', $post_data->id) }}" class="btn btn-primary" style="display:block;width:100px;">詳細</a>
+                <a href="{{ route('booking.show', [$post_data->id]) }}" class="btn btn-primary" style="display:block;width:100px;">詳細</a>
 
 
             </div>
@@ -22,8 +31,10 @@
         </div>
 
         @endforeach
+        <div>
+        {{ $register_data->appends(request()->input())->links() }}
+        </div>
     </div>
 </div>
-
 
 @endsection
