e0e6bb1b294162a52cf4020882406e621deabe82
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-12-08 23:28:31 +0900

    feature ようこそ画面作成

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">マイページ</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    <!-- ようこそ画面 -->
+                    <p>ようこそ、{{Auth::user()->name}}さん！</p>
                 </div>
             </div>
         </div>
