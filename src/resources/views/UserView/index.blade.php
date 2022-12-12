06894b569833e2c6b7ed47f30001c874a4c947a7
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-12-12 12:01:42 +0900

    feature ユーザーレイアウト修正

--- src/resources/views/UserView/index.blade.php
+++ src/resources/views/UserView/index.blade.php
@@ -1,40 +1,32 @@
 @extends('layouts.app')
 @extends('footer.users_footer')
 @section('content')
+<link href="{{ asset('css/user/index.css') }}" rel="stylesheet">
 <h1>レジャー一覧</h1>
-<form method="GET" action="{{ route('booking.index') }}">
 
-    <input type="search" placeholder="場所を入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
-    <br />
-    <br />
-    <input type="search" placeholder="ジャンルを入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
-    <div>
-    <input type="submit" value="検索する" class="btn btn-success">
-    </div>
-</form>
-<div class="card-wrapper" style="display:flex;justify-content:center;">
-    <div class="card-container" style="display:flex;gap:40px;width:1000px;flex-wrap:wrap;">
+<div class="card-wrapper">
+    <div class="card-container">
 
         @foreach ($register_data as $post_data)
-        <div class="card" style="width: 200px;display:flex;align-items: center;">
+        <div class="card">
             <img src="{{ Storage::url($post_data->image_path) }}" style="width:100px;" />
             <div class="card-body">
                 <div class="">場所:{{$post_data->prefecture_name}}</div>
                 <div class="">ジャンル:{{$post_data->genle_name }}</div>
                 <br />
-
-                <a href="{{ route('booking.show', [$post_data->id]) }}" class="btn btn-primary" style="display:block;width:100px;">詳細</a>
-
+                <a href="{{ route('booking.show', [$post_data->id]) }}" class="btn btn-outline-primary" style="display:block;width:100px;">詳細</a>
 
             </div>
 
         </div>
 
         @endforeach
+
         <div>
-        {{ $register_data->appends(request()->input())->links() }}
+            {{ $register_data->appends(request()->input())->links() }}
         </div>
     </div>
 </div>
+
 
 @endsection
