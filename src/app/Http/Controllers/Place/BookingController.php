5ed87403255e800a0dcf98cec70261c57851f2e2
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-12-12 11:48:03 +0900

    feature コメント追加

--- src/app/Http/Controllers/Place/BookingController.php
+++ src/app/Http/Controllers/Place/BookingController.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers\Place;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Register;
-
+// 認証機能追加
 class BookingController extends Controller
 {
     public function __construct()
@@ -20,34 +20,34 @@ class BookingController extends Controller
      */
     public function index(Request $request)
     {
-        
+
         $register_data = Register::paginate(10);
         $search = $request->input('search');
         $query = Register::query();
         if ($search) {
-            
+
             // 全角スペースを半角に変換
             $spaceConversion = mb_convert_kana($search, 's');
-          
+
             // 単語を半角スペースで区切り、配列にする
             $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
-          
+
 
             // 単語をループで回し、部分一致するものがあれば、$queryとして保持される
             foreach ($wordArraySearched as $value) {
                 $query->where('prefecture_name', 'like', '%' . $value . '%')
                     ->orwhere('genle_name', 'like', '%' . $value . '%');
             }
-           
+
             $register_data = $query->paginate(10);
-            
-            
+
+
         }
-      
+
         return view('UserView.index', compact('register_data'));
 
     }
-
+    
     /**
      * Show the form for creating a new resource.
      *
