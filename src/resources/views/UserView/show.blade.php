0da5853740197b018484157d3541094036519bb6
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-12-12 12:02:19 +0900

    feature ユーザーレイアウト修正

--- src/resources/views/UserView/show.blade.php
+++ src/resources/views/UserView/show.blade.php
@@ -1,39 +1,59 @@
 @extends('layouts.app')
+
 @extends('footer.users_footer')
 
 @section('content')
-<h1>詳細ページ</h1>
-<div class="container">
-    <div class="row justify-content-start">
-    </div>
 
-    <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px; background-color: lightgray; color: black;width:800px;">
-        <div class="col-md-3">
-            <img src="{{asset('storage/'.$register_data->image_path)}}" class="img-thumbnail" style=" border:1px solid #333333;width:100px;" />
+<link href="{{ asset('css/user/show.css') }}" rel="stylesheet">
 
-            <div class="col-md-5">
-                <label class="form-label">場所</label>
-                <input type="text" value="{{ $register_data->prefecture_name  }}" name="prefecture_name" class="form-control" readonly>
+<div class="container m-6">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header" >詳細確認ページ</div>
+
+                <div class="card-body">
+                    <div class="col-mb-9">
+                        <img src="{{asset('storage/'.$register_data->image_path)}}" class="rounded"/>
+                        <div class="form-group">
+                            <label for="name">場所</label>
+                            <div>
+                                <input type="text" value="{{ $register_data->prefecture_name  }}" name="prefecture_name" class="form-control" readonly>
+                                
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">ジャンル</label>
+                        <div>
+                            <input type="text" value="{{ $register_data->genle_name  }}" name="genle_name" class="form-control" readonly>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">人数</label>
+                        <div>
+                            <input type="text" value="{{ $register_data->reservation_number_of_people }}" name="reservation_number_of_people" class="form-control"  readonly>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">日付</label>
+                        <div>
+
+                            <input type="text" value="{{ $register_data->reservation_date}}" name="reservation_date" class="form-control"  readonly>
+                        </div>
+                    </div>
+
+                    <div class="input-group md-3">
+                        <a href="{{ route('booking.create', $register_data->id) }}" class="btn btn-outline-info" style="padding:5px;margin:10px;">予約へ進む</a>
+                    </div>
+                </div>
             </div>
         </div>
-        <br />
-        <div class="col-md-3">
-            <label class="form-label">ジャンル</label>
-            <input type="text" value="{{ $register_data->genle_name  }}" name="genle_name" class="form-control" readonly>
-        </div>
-        <br />
-        <div class="col-md-3">
-            <label class="form-label">人数</label>
-            <input type="text" value="{{ $register_data->reservation_number_of_people }}" name="reservation_number_of_people" class="form-control" readonly>
-        </div>
-        <br />
-        <div class="col-md-3">
-            <label class="form-label">日付</label>
-
-            <input type="text" value="{{ $register_data->reservation_date}}" name="reservation_date" class="form-control" readonly>
-        </div>
-        <br />
-        <div class="input-group mb-3">
-            <a href="{{ route('booking.create', $register_data->id) }}" class="btn btn-info">予約へ進む</a>
+    </div>
 </div>
+
 @endsection
